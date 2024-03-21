0aa9ef4e7709ab5b7f3d8418897d2ff85d02516f
Rajesh Choudhari <48985833+rajeshchoudhari147@users.noreply.github.com>
2024-03-22 10:26:29 +1100

    chore: add tanstack

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,19 +1,27 @@
-import { FlatList, StyleSheet, Text } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet, Text } from 'react-native';
 
-import { View } from "@/components/Themed";
-import { useEffect, useState } from "react";
-import { fetchTopRatedMovies } from "@/api/movies";
+import { View } from '@/components/Themed';
+import { fetchTopRatedMovies } from '@/api/movies';
+import { useQuery } from '@tanstack/react-query';
 
 export default function TabOneScreen() {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const movies = fetchTopRatedMovies();
-      setMovies(await movies);
-    };
-    fetchMovies();
+  const {
+    data: movies,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['movies'],
+    queryFn: fetchTopRatedMovies,
   });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>{error?.message}</Text>;
+  }
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -31,8 +39,8 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 20,
