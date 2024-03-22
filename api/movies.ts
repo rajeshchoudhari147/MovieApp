88c7cc58ccf5146087c2bcca3e32046351f8c82d
Rajesh Choudhari <48985833+rajeshchoudhari147@users.noreply.github.com>
2024-03-22 16:57:03 +1100

    chore: add infinite scroll with react query

--- api/movies.ts
+++ api/movies.ts
@@ -5,9 +5,12 @@ const headers = {
   Authorization: 'Bearer ' + apiKey,
 };
 
-export const fetchTopRatedMovies = async () => {
-  const url =
-    'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
+export const fetchTopRatedMovies = async ({
+  pageParam,
+}: {
+  pageParam: number;
+}) => {
+  const url = `https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${pageParam}`;
   const options = {
     method: 'GET',
     headers,
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,17 +2,15 @@ import { ActivityIndicator, FlatList, StyleSheet, Text } from 'react-native';
 
 import { View } from '@/components/Themed';
 import { fetchTopRatedMovies } from '@/api/movies';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import MovieListItem from '@/components/MovieListItem';
 
 export default function TabOneScreen() {
-  const {
-    data: movies,
-    isLoading,
-    error,
-  } = useQuery({
+  const { data, isLoading, error, fetchNextPage } = useInfiniteQuery({
     queryKey: ['movies'],
     queryFn: fetchTopRatedMovies,
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => pages.length + 1,
   });
 
   if (isLoading) {
@@ -23,6 +21,8 @@ export default function TabOneScreen() {
     return <Text>{error?.message}</Text>;
   }
 
+  const movies = data?.pages.flat();
+
   return (
     <View style={styles.container}>
       <FlatList
@@ -31,6 +31,9 @@ export default function TabOneScreen() {
         contentContainerStyle={{ gap: 5 }}
         columnWrapperStyle={{ gap: 5 }}
         renderItem={({ item }) => <MovieListItem movie={item} />}
+        onEndReached={() => {
+          fetchNextPage();
+        }}
       />
     </View>
   );
