8cba1208190a251fdde42ee24932c6532b093289
Rajesh Choudhari <48985833+rajeshchoudhari147@users.noreply.github.com>
2024-03-22 12:08:30 +1100

    chore: fetch movie detail by id

--- api/movies.ts
+++ api/movies.ts
@@ -1,20 +1,41 @@
+const apiKey =
+  'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNmNjMzVhNzI1NDVmOGM0OTc4YjI5YTA1MDE2OWRmYSIsInN1YiI6IjY1ZmJiYjI4N2Y2YzhkMDE3YzZiZmUwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xvasry-70dc8UZ5tfGcq5cZP9m4Hte9JZG6vUOsnCzk';
+const headers = {
+  accept: 'application/json',
+  Authorization: 'Bearer ' + apiKey,
+};
+
 export const fetchTopRatedMovies = async () => {
   const url =
-    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
+    'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1';
   const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNmNjMzVhNzI1NDVmOGM0OTc4YjI5YTA1MDE2OWRmYSIsInN1YiI6IjY1ZmJiYjI4N2Y2YzhkMDE3YzZiZmUwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xvasry-70dc8UZ5tfGcq5cZP9m4Hte9JZG6vUOsnCzk",
-    },
+    method: 'GET',
+    headers,
   };
 
-  try {
-    const response = await fetch(url, options);
-    const json = await response.json();
-    return json.results;
-  } catch (error) {
-    console.error("error:" + error);
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch movies');
   }
+
+  const json = await response.json();
+  return json.results;
+};
+
+export const fetchMovie = async (id: number) => {
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: 'GET',
+    headers,
+  };
+
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch movie with id ${id}`);
+  }
+
+  const json = await response.json();
+  return json;
 };
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,12 +1,32 @@
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator } from 'react-native';
 import React from 'react';
 import { useLocalSearchParams } from 'expo-router';
+import { useQuery } from '@tanstack/react-query';
+import { fetchMovie } from '@/api/movies';
 
 export default function MovieDetails() {
   const { id } = useLocalSearchParams();
+
+  const {
+    data: movie,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['movies', id],
+    queryFn: () => fetchMovie(id),
+  });
+
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch movie</Text>;
+  }
+
   return (
     <View>
-      <Text>MovieDetails: {id}</Text>
+      <Text style={{ fontSize: 24, fontWeight: '500' }}>{movie.title}</Text>
     </View>
   );
 }
